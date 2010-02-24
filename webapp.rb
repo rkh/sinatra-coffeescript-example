0ab02432617679c43e26e937c338ec8cf491276b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-02-24 18:12:05 +0100

    don't enforce client compiler

--- webapp.rb
+++ webapp.rb
@@ -1,9 +1,5 @@
 require "sinatra"
-require "haml"
 require "haml-more"
-
-# enforce client compiler
-Haml::More::CoffeeScript.compiler = Haml::More::CoffeeScript::ClientCompiler.new
 
 get("/") { haml :coffee_script }
 get("/source") { haml :source }
